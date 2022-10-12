1234659206f43864bf2945eeb9601c634f34703f
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-10-12 12:15:04 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { LoadMore } from './Button/Button';
 import { Box } from './common/Box';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ToastContainer } from 'react-toastify';
+import { Loader } from './Loader/Loader';
+import { getPhoto } from 'services/Api';
+import { toast } from 'react-toastify';
 
 export const App = () => {
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
+  const [gallery, setGallery] = useState([]);
+  const [isLoader, setIsLoader] = useState(false);
   const [isLoadMore, setIsLoadMore] = useState(false);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    setIsLoader(true);
+    getPhoto(query, page)
+      .then(data => {
+        setGallery(collect => [...collect, ...data.hits]);
+        if (data.hits.length) {
+          onLoadMore();
+        } else {
+          resetGallery();
+          offLoadMore();
+          toast.error(`По запросу "${query}" ничего не найдено`);
+        }
+        return data;
+      })
+      .then(data => {
+        if (data.hits.length && data.hits.length < 12) {
+          offLoadMore();
+          toast('Коллекция закончилась');
+        }
+      })
+      .catch(error => setError(error.massage))
+      .finally(() => setIsLoader(false));
+  }, [query, page]);
+
+  const resetGallery = () => {
+    setGallery([]);
+  };
   const getQuery = newQuery => {
-    setQuery(state => newQuery);
+    setQuery(newQuery);
     setPage(1);
+    setGallery([]);
   };
 
   const loadMore = () => {
@@ -42,13 +79,17 @@ export const App = () => {
 
       <ToastContainer />
       <Searchbar onSubmit={getQuery} />
-
-      <ImageGallery
-        query={query}
-        page={page}
-        onLoad={onLoadMore}
-        offLoad={offLoadMore}
-      />
+      {isLoader && (
+        <Box display="flex" justifyContent="center">
+          <Loader />
+        </Box>
+      )}
+      <ImageGallery gallery={gallery} />
+      {error && (
+        <Box display="flex" justifyContent="center">
+          <p>{error}</p>
+        </Box>
+      )}
       {isLoadMore && (
         <Box display="flex" justifyContent="center">
           <LoadMore onClick={loadMore}>Load More</LoadMore>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,58 +1,18 @@
 import { Gallery, ModalImage } from './ImageGallery.styled';
-import { useState, useEffect } from 'react';
-import { getPhoto } from 'services/Api';
+import { useState } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Loader } from '../Loader/Loader';
-import { Box } from '../common/Box';
 import { Modal } from 'components/Modal/Modal';
-import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const ImageGallery = ({ query, page, onLoad, offLoad }) => {
-  const [gallery, setGallery] = useState([]);
+export const ImageGallery = ({ gallery }) => {
   const [modalImg, setModalImg] = useState('');
-  const [isLoader, setIsLoader] = useState(false);
   const [isModal, setIsModal] = useState(false);
-  const [error, setError] = useState(null);
 
   const handelClick = img => {
     setIsModal(state => !state);
     setModalImg(state => (state = img));
   };
-
-  const resetGallery = () => {
-    setGallery([]);
-  };
-
-  useEffect(() => {
-    if (!query) {
-      return;
-    }
-    setIsLoader(true);
-    getPhoto(query, page)
-      .then(data => {
-        setGallery(collect => [...collect, ...data.hits]);
-        if (data.hits.length) {
-          onLoad();
-        } else {
-          resetGallery();
-          offLoad();
-          toast.error(`По запросу "${query}" ничего не найдено`);
-        }
-        return data;
-      })
-      .then(data => {
-        if (!data.hits.length || data.hits.length < 12) {
-          offLoad();
-          toast('Коллекция закончилась');
-        } else {
-          onLoad();
-        }
-      })
-      .catch(error => setError(error.massage))
-      .finally(() => setIsLoader(false));
-  }, [query, page]);
 
   return (
     <>
@@ -66,11 +26,7 @@ export const ImageGallery = ({ query, page, onLoad, offLoad }) => {
           />
         ))}
       </Gallery>
-      {isLoader && (
-        <Box display="flex" justifyContent="center">
-          <Loader />
-        </Box>
-      )}
+
       {isModal && (
         <Modal onClose={handelClick}>
           <ModalImage src={modalImg} alt="" />
@@ -81,8 +37,5 @@ export const ImageGallery = ({ query, page, onLoad, offLoad }) => {
 };
 
 ImageGallery.protoTypes = {
-  query: PropTypes.string.isRequired,
-  page: PropTypes.number.isRequired,
-  onLoad: PropTypes.func,
-  offLoad: PropTypes.func,
+  gallery: PropTypes.array.isRequired,
 };
